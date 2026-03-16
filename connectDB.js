9b9d10a398e6190efb0669f7624a4ae6f1695306
kaveri <kaverimetri07@gmail.com>
2026-03-16 17:39:22 +0530

    fixed mongodbURL issue

--- connectDB.js
+++ connectDB.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 export async function connectDB() {
   try {
     console.log(process.env.MONGODB_URL)
-    const connection = await mongoose.connect(`${process.env.MONGODB_URL}`)
+    const connection = await mongoose.connect(`${process.env.MONGODB_URL}/underwriting`)
     console.log('Connected to MongoDB')
   } catch (error) {
     console.error('MongoDB connection error:', error)
