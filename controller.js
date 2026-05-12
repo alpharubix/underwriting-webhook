7ce3902c583c90a093d3a09d23e0a784ba1e4b75
Ashok <ashokmommyguy@gmail.com>
2026-05-12 14:39:42 +0530

    added webhook-consumer endpoint for bankstatement

--- controller.js
+++ controller.js
@@ -1,10 +1,16 @@
-import { BankStatement } from "./models.js";
 
 export async function saveWebhookResponse(req, res) {
   let response_body = req.body;
   if (response_body) {
-    console.log(response_body)
-    BankStatement.create(response_body)
+    const bankstatementStatementWebhookConsumerUrl =
+  "https://underwritting-tool-backend-972437264901.us-east1.run.app/v1/bsa/webhook-response-handler";
+    await fetch(bankstatementStatementWebhookConsumerUrl, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify(response_body),
+});
     return res.json(response_body)
   }
 }
