d8b15f44e890f9e5bf43067e1186c1ce3593b064
Ashok <ashokmommyguy@gmail.com>
2026-05-12 15:08:13 +0530

    done chanes in port variable for deployment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import "dotenv/config"
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8080
 import cors from 'cors'
 import express from 'express'
 import { connectDB } from './connectDB.js'
